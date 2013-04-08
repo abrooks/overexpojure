989387220771541a28c1db324407069cab655a05
Matt Oquist <moquist@majen.net>
2013-04-08 15:36:05 -0400

    Change how html is being traversed using un-html.
    The new format is easier to process with standard Clojure. This sets us up for
    writing a proper colspan expander.

--- src/overexpojure/core.clj
+++ src/overexpojure/core.clj
@@ -1,18 +1,55 @@
 (ns overexpojure.core
-  (require [pl.danieljanus.tagsoup :as ts]))
+  (require [pl.danieljanus.tagsoup :as ts]
+           [clojure.walk :refer [prewalk]]))
 
 (def e (ts/parse "file:ClojureConj2012.html"))
 (def f (ts/parse "file:ClojureWest2012.html"))
 
-(defn find-first [[tag attr & content] name]
-  (some #(when (= name (first %))
-           %)
-        content))
+(defn meta-ize [[tag attr & cont]]
+  (with-meta
+    (into [] cont)
+    {:tag tag :attr attr}))
+
+(defn un-html [html]
+  (prewalk #(if (vector? %)
+              (meta-ize %)
+              %)
+           html))
+
+(defn get-tag [e]
+  (:tag (meta e)))
+
+(defn get-attrs [e]
+  (:attr (meta e)))
+
+(defn get-attr [e k]
+  (get (get-attrs e) k))
 
 (defn html->
   ([html] html)
   ([html fpath & rpath]
-     (apply html-> (find-first html fpath) rpath)))
+     (apply html->
+            (first (filter #(= fpath (get-tag %)) html))
+            rpath)))
+
+(defn ex-span-d [tr]
+
+#_(binding [*print-meta* true]
+    (doseq [tr (html-> (un-html f) :body :table :tbody)
+            td tr
+            :when (= "3" (get-attr td :colspan))]
+      (prn td)))
+
+  )
+
+
+(comment
+  ;; Starts with :html
+  ;; How-to-run comments here
+  (html-> e :body :table :tbody)
+  (html-> e :body :table :tbody :tr :td)
+
+)
 
 {:conferences {:conj-2010
                {:name "Clojure/conj 2010"
@@ -36,9 +73,4 @@
           :links {:video "http://www.youtube.com/watch?v=NnSpaR67hXg"}
           :tags #{:history}}]}
 
-(comment
-  ;; Starts with :html
-  ;; How-to-run comments here
-  (html-> e :body :table :tbody)
-  (html-> e :body :table :tbody :tr :td)
-)
+true
