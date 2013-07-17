0f9dc28977a8687c6cbfaf9102480d453b2dcec4
Matt Oquist <moquist@majen.net>
2013-07-17 16:33:52 -0400

    update user.clj with necessary namespaces and create/stop/reset fns

--- dev/user.clj
+++ dev/user.clj
@@ -1,4 +1,20 @@
 (ns user
   (:require [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-            [clojure.repl :refer :all]))
+            [clojure.repl :refer :all]
+            [overexpojure.core :as oec]
+            [overexpojure.datomic :as oed]
+            [datomic.api :as d :refer [db q]]))
 
+(defn create
+  "Creates and stores a new datomic mem-db instance."
+  []
+  (d/create-database oed/uri))
+
+(defn stop
+  "Shuts down the application and releases resources."
+  []
+  (d/delete-database oed/uri))
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/create))
